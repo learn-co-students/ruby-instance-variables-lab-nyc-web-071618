a032cf3fedb01d82a869f2997733f36196581323
Your Name <you@example.com>
2018-07-18 10:07:51 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,11 @@
+class Dog
+ def name=(dogs_name)
+  @this_dogs_name = dogs_name
+  #this name = method sets arguement equal 2 varible
+end
+ def name
+   @this_dogs_name
+   #this method returns our name
+
+end
+end
